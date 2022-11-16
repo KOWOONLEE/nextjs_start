8803437549d9fc0721c967b47988caf5e15c8907
KOWOONLEE <hhh3901@gmail.com>
2022-11-16 13:00:27 +0900

    Rev: ssr

--- pages/index.js
+++ pages/index.js
@@ -4,22 +4,11 @@ import { useEffect, useState } from "react";
 
 // const API_KEY = "0228158c96d8626eb12b82af4b710b51";
 
-export default function Home() {
-  const [movies, setMovies] = useState([]);
-
-  useEffect(() => {
-    (async () => {
-      const { results } = await (await fetch(`/api/movies`)).json();
-      console.log(results);
-      setMovies(results);
-    })();
-  }, []);
-
+export default function Home({ results }) {
   return (
     <div className="container">
       <Seo title="Home" />
-      {!movies && <h4>Loading...</h4>}
-      {movies?.map((movie) => (
+      {results?.map((movie) => (
         <div className="movie" key={movie.id}>
           <img src={`https://image.tmdb.org/t/p/w500/${movie.backdrop_path}`} />
           <h4>{movie.original_title}</h4>
@@ -48,4 +37,15 @@ export default function Home() {
       `}</style>
     </div>
   );
+}
+//이름 중요 바꾸면 안됨. 백엔드에서 실행(서버에서 실행)
+export async function getServerSideProps() {
+  const { results } = await (
+    await fetch(`http://localhost:3007/api/movies`)
+  ).json();
+  return {
+    props: {
+      results,
+    },
+  };
 }
